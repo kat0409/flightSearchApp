2a6f15ca8e1e05226e3b65d7a30a413b69c9b395
jxnfernandx <orangutangklan@gmail.com>
2024-02-10 15:36:52 -0600

    added html/css/js

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// Array to hold all destination info //
+const destinationArray = [];
+
+// Card Class to assign info from API calls //
+class Card {
+    constructor(image, name, budget, airline) {
+        this.image = image;
+        this.name = name;
+        this.budget = budget;
+    }
+}
+
+const destinationInput = document.getElementById("destination");
+const budgetInput = document.getElementById("budget");
+
+// Function to fetch flight info from API //
+const fetchFlightInfo = async () => {
+
+}
+
