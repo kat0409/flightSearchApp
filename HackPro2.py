7219e232c0c20060a9973f744c164607a73197a2
kat0409 <147408482+kat0409@users.noreply.github.com>
2024-10-15 22:21:02 -0500

    Update HackPro2.py

--- HackPro2.py
+++ HackPro2.py
@@ -9,7 +9,6 @@ patterns = [
     (r'(.*) your name?', ["I'm just a chatbot, I don't have a name."]),
     (r'quit', ['Bye, take care!', 'Goodbye!']),
     (r'ticket price|cheapest tickets| most affordable flights', ['Prices range from 6 cheapest categories. The price ranges are $25-$100, $101-$500, $501-$1000, $1001-$5000, $5001-$10000, $1001+']),
-    ()
 ]
 
 # Create a chatbot
